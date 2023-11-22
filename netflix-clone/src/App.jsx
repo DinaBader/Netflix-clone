36d8e371b6bb4ddbb2d72b34a7765d57a7f30b35
manso21 <alimanso234@gmail.com>
2023-11-23 00:18:49 +0200

    preparing LandingPage Route

--- netflix-clone/src/App.jsx
+++ netflix-clone/src/App.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import MoviesBrowse from "./pages/MoviesBrowse";
 import MovieDetailsPage from "./pages/MovieDetails";
+import LandingPage from "./pages/LandingPage";
+
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 const App = () => {
@@ -8,7 +10,8 @@ const App = () => {
     <div>
       <BrowserRouter>
         <Routes>
-          <Route element={<MoviesBrowse />} path="/" />
+          <Route element={<LandingPage />} path="/" />
+          <Route element={<MoviesBrowse />} path="/browse-movies" />
           <Route element={<MovieDetailsPage />} path="/movie-details/:id" />
         </Routes>
       </BrowserRouter>
--- netflix-clone/src/components/common/Nav/index.jsx
+++ netflix-clone/src/components/common/Nav/index.jsx
@@ -1,15 +1,18 @@
 import React from "react";
 import "./style.css";
+import { Link } from "react-router-dom";
 
 const Nav = () => {
   return (
     <div className="nav">
       <div className="nav-contents">
-        <img
-          src="https://bull-headed-bear.com/wp-content/uploads/2022/08/image-18-1024x276.png"
-          alt=""
-          className="nav-logo"
-        ></img>
+        <Link to="/">
+          <img
+            src="https://bull-headed-bear.com/wp-content/uploads/2022/08/image-18-1024x276.png"
+            alt=""
+            className="nav-logo"
+          ></img>
+        </Link>
 
         <div className="nav-right">
           <p>UNLIMITED TV SHOWS & MOVIES</p>
